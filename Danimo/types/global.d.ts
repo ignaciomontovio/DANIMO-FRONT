78adc290a98e4fe28bf5414ce31942be88a78952
Martin-Roberto-lecuona <robertlecuona@gmail.com>
2025-05-12 20:39:50 -0300

    first add

---
+++ Danimo/types/global.d.ts
@@ -0,0 +1,10 @@
+declare module '*.png' {
+    const content: any;
+    export default content;
+  }
+
+// Puedes agregar cualquier tipo global aquí
+// type User = {
+//     id: string;
+//     name: string;
+//   };
