398ce5c651179d927d4a91ce57c5753f836da4ff
StefiVioli <stefaniavioliali97@gmail.com>
2025-06-16 14:22:16 +0200

    Maquetado perfil

--- DanimoApp/app/tabs/menu.tsx
+++ DanimoApp/app/tabs/menu.tsx
@@ -3,7 +3,7 @@ import { useUserLogInStore } from "@/stores/userLogIn";
 import { FontAwesome } from "@expo/vector-icons";
 import { router } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 
 export default function profile() {
@@ -24,7 +24,7 @@ export default function profile() {
       end={{ x: 0, y: 1 }}
       className="w-full h-full"
     >
-      <View className="flex-1 items-center justify-start space-y-15 px-6"> 
+      <View className="flex-1 items-center justify-start space-y-15 px-6">
         <View className="w-full flex-row items-center justify-center gap-2 px-2 py-6">
           {/* Ícono de usuario */}
           <View className="rounded-full p-4 border-2 border-oscuro bg-colo2 shadow-md">
@@ -36,18 +36,22 @@ export default function profile() {
             <View className="bg-fondo px-4 py-2 rounded-full border border-oscuro shadow-sm">
               <Text className="text-oscuro text-center font-bold text-lg">Juan Pérez</Text>
             </View>
-            <Text className="text-right text-oscuro  font-bold mt-1 text-base text-xl">Perfil &gt; </Text>
+            <Pressable onPress={() => router.replace("/profile")}>
+              <Text className="text-right text-oscuro font-bold mt-1 text-xl">
+                Perfil &gt;
+              </Text>
+            </Pressable>
+          </View>
         </View>
-      </View>
 
-        <ButtonLight_small onPress={()=>("")} text="Recomendacion"/>
-        <ButtonLight_small onPress={()=>("")} text="Rutinas"/>
-        <ButtonLight_small onPress={()=>("")} text="Estadisticas"/>
-        <ButtonLight_small onPress={()=>("")} text="Eventos Significativos"/>
-        <ButtonLight_small onPress={()=>(router.replace("/emergencyContact"))} text="Contactos de Emergencia"/>
-        <ButtonLight_small onPress={()=>("")} text="Profesionales Ascociados"/>
-        <ButtonLight_small onPress={()=>("")} text="Medicacion"/>
-        <ButtonDark_small onPress={handleLogoff} text="Cerrar Sesion"/>
+        <ButtonLight_small onPress={() => ("")} text="Recomendación" />
+        <ButtonLight_small onPress={() => ("")} text="Rutinas" />
+        <ButtonLight_small onPress={() => ("")} text="Estadísticas" />
+        <ButtonLight_small onPress={() => ("")} text="Eventos Significativos" />
+        <ButtonLight_small onPress={() => (router.replace("/emergencyContact"))} text="Contactos de Emergencia" />
+        <ButtonLight_small onPress={() => ("")} text="Profesionales Ascociados" />
+        <ButtonLight_small onPress={() => (router.replace("/medication"))} text="Medicación" />
+        <ButtonDark_small onPress={handleLogoff} text="Cerrar Sesión" />
       </View>
     </LinearGradient>
   );
