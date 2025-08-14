e5cfc41b53365c4c4f2b8b53eb0584941ab2b98e
Martin-Roberto-lecuona <robertlecuona@gmail.com>
2025-08-13 21:18:21 -0300

    integracion de resumenes

--- DanimoApp/app/screensOnlyProf/historialDeChat.tsx
+++ DanimoApp/app/screensOnlyProf/historialDeChat.tsx
@@ -1,13 +1,50 @@
-import { ButtonDark } from "@/components/buttons";
+import { ButtonAccept, ButtonDark } from "@/components/buttons";
 import HeaderGoBack from "@/components/headerGoBack";
-import { colors } from "@/stores/colors"; // Asegúrate que tengas estos colores
-import { router } from "expo-router";
-import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { colors } from "@/stores/colors";
+import { URL_BASE, URL_DANI } from "@/stores/consts";
+import { patientProfile, usePatientStore } from "@/stores/patientStore";
+import { useUserLogInStore } from "@/stores/userLogIn";
+import { router, useLocalSearchParams } from "expo-router";
+import React, { useCallback, useEffect, useState } from "react";
+import { Alert, ScrollView, Text, View } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function HistorialDeChat() {
+  const token = useUserLogInStore((state) => state.token);
+  const [weekResume, setWeekResume] = useState("");
+  const { patientId } = useLocalSearchParams<{ patientId: string }>();
+  const patients = usePatientStore((state: { patients: any; }) => state.patients);
+  const patient = patients.find((p:patientProfile) => p.id === patientId);
+  
+  const fetchData = useCallback(async () => {
+      try {
+        const response = await fetch(URL_BASE + URL_DANI + "/weeklySummary", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+          body: JSON.stringify({ userId: patientId }),
+        });
+        
+        if (!response.ok && response.status !== 500) {
+          const errorText = await response.json();          
+          throw new Error(errorText.error);
+        }
+        
+        const data = await response.json();
+        setWeekResume(data.summary)
+      } catch (error) {       
+        console.error("Error al obtener resumen de paciente:", error);
+        Alert.alert("Error", "No se pudo obtener resumen de paciente.");
+      } 
+    }, [patientId, token]);
+
+  useEffect(() => {
+      fetchData();
+    }, [fetchData]);
+  
   return (
     <SafeAreaProvider>
       <LinearGradient
@@ -17,28 +54,44 @@ export default function HistorialDeChat() {
           className="w-full h-full"
         >
           <HeaderGoBack
-            text="Resumen"
+            text={"Resumen " + patient.firstName + " " + patient.lastName }
             onPress={() => router.replace("/profesional/home")}
           />
           
           <ScrollView className="flex-1 px-5 py-5">
             {/* Tarjeta resumen */}
-            <View className="bg-white/90 rounded-2xl p-4 shadow-md">
-              <Text className="text-center text-base font-bold text-pink-500 mb-2">
+            <View className="bg-fondo rounded-2xl p-4 shadow-md">
+              <Text className="text-center text-xl font-bold text-pink-500 mb-2">
                 Semana
               </Text>
-              <Text className="text-sm text-black text-justify">
-                Explorar recursos internos y externos para mejorar. Se recomienda fortalecer hábitos de descanso, espacios de contención emocional y actividades que refuercen la autoestima.
+              {weekResume === "" && 
+                  <View className="self-center bg-gray-200 px-4 py-2 rounded-full my-2">
+                    <Text className="text-gray-600 italic">Generando Resumen ...</Text>
+                  </View>
+              }
+              <Text className="text-sm text-oscuro text-left font-bold">
+                {weekResume}
               </Text>
             </View>
             
             <View className="justify-end mt-8 space-y-4">
-              <ButtonDark text="Detalle" onPress={() => console.log("Ver detalles")} />
-              <ButtonDark text="Personalizado" onPress={() => console.log("Ver detalles")} />
-              <ButtonDark text="Historico" onPress={() => console.log("Ver historico")} />
+              <ButtonAccept text="Reportes" onPress={() => console.log("Ver detalles")} />
+              <ButtonDark text="Personalizado" onPress={() => {
+                router.push({
+                    pathname: "/screensOnlyProf/personaliseResume",
+                    params: { patientId },
+                });
+                }} />
+              <ButtonDark text="Historico" onPress={() => {
+                router.push({
+                    pathname: "/screensOnlyProf/historicPatient",
+                    params: { patientId },
+                });
+                }} />
             </View>
           </ScrollView>
       </LinearGradient>
     </SafeAreaProvider>
   );
 }
+
---
+++ DanimoApp/app/screensOnlyProf/personaliseResume.tsx
@@ -0,0 +1,106 @@
+import { ButtonDark_small } from "@/components/buttons";
+import HeaderGoBack from "@/components/headerGoBack";
+import { Input_date } from "@/components/input";
+import { colors } from "@/stores/colors";
+import { URL_BASE, URL_DANI } from "@/stores/consts";
+import { useUserLogInStore } from "@/stores/userLogIn";
+import { router, useLocalSearchParams } from "expo-router";
+import React, { useState } from "react";
+import { Alert, ScrollView, Text, View } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import Markdown from 'react-native-markdown-display';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function PersonaliseResume() {
+  const token = useUserLogInStore((state) => state.token);
+  const [fullResume, setFullResume] = useState("Ingrese fecha de inicio y fin para obtener un resumen.");
+  const [start, setStart] = useState<Date | undefined>(undefined);
+  const [end, setEnd] = useState<Date | undefined>(undefined);
+  const { patientId } = useLocalSearchParams<{ patientId: string }>();
+  
+  const getResume = async () => {
+    
+      try {
+        const response = await fetch(URL_BASE + URL_DANI + "/summary", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+          body: JSON.stringify({ 
+            userId: patientId, 
+            startDate: start?.toISOString().split("T")[0], 
+            endDate: end?.toISOString().split("T")[0] }
+          ),
+        });
+        
+        if (!response.ok && response.status !== 500) {
+          const errorText = await response.json();          
+          // const errorText = await response.text();          
+          throw new Error(errorText.error);
+        }
+  
+        const data = await response.json();
+        setFullResume(data.summary)
+      } catch (error) {       
+        console.error("Error al obtener pacientes:", error);
+        Alert.alert("Error", "No se pudo obtener la lista de pacientes.");
+      } 
+    }
+  
+  return (
+    <SafeAreaProvider>
+      <LinearGradient
+          colors={[colors.color5, colors.fondo]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          className="w-full h-full"
+        >
+          <HeaderGoBack
+            text="Resumen Personalizado"
+            onPress={() =>{
+              router.push({
+                pathname: "/screensOnlyProf/historialDeChat",
+                params: { patientId },
+              });}}
+          />
+          
+          <ScrollView className="flex-1 px-5 py-5">
+            {/* elejir fechas */}
+            <View className="p-6 bg-fondo rounded-2xl mb-5">
+              <View className="py-2 bg-color1 rounded-2xl mb-2">
+                <Text className="text-xl font-bold text-white text-center">Inicio</Text>
+              </View>
+              <Input_date setDate={setStart} date={new Date()} />
+              <View className="py-2 bg-color1 rounded-2xl mb-2 mt-5">
+                <Text className="text-xl font-bold text-white text-center">Fin</Text>
+              </View>
+              <Input_date setDate={setEnd} date={new Date()} />
+              <ButtonDark_small
+                  text="Obtener Resumen"
+                  onPress={getResume}
+                />
+            </View>
+            <View className="bg-fondo rounded-2xl p-4 shadow-md mb-10">
+              <Text className="text-center text-xl font-bold text-pink-500 mb-2">
+                Resumen
+              </Text>
+              {/* <Text className="text-sm text-black text-left">
+                {fullResume}
+              </Text> */}
+
+              <Markdown style={{
+                body: { color: 'black', fontSize: 14 },
+                heading1: { fontSize: 22, fontWeight: 'bold', color: '#E91E63' },
+                bullet_list: { marginVertical: 5 },
+              }}>
+                {fullResume}
+              </Markdown>
+
+            </View>
+          </ScrollView>
+          
+      </LinearGradient>
+    </SafeAreaProvider>
+  );
+}
